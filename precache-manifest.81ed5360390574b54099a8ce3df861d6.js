c6bdf72f74241e33967bcf4df13cbb8cf78b7b46
hanly <hanjimj000@gmail.com>
2020-05-19 23:58:38 +0700

    Updates

---
+++ precache-manifest.81ed5360390574b54099a8ce3df861d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61db47094a44f2e5a49daf427954fe87",
+    "url": "/lygiahan/DeloyGit.git/index.html"
+  },
+  {
+    "revision": "c99e96438c53b3799685",
+    "url": "/lygiahan/DeloyGit.git/static/css/2.98775090.chunk.css"
+  },
+  {
+    "revision": "6a60d3ac9fd5ff3fe8b9",
+    "url": "/lygiahan/DeloyGit.git/static/css/main.0bf7335f.chunk.css"
+  },
+  {
+    "revision": "c99e96438c53b3799685",
+    "url": "/lygiahan/DeloyGit.git/static/js/2.a0648e96.chunk.js"
+  },
+  {
+    "revision": "fe65c7f2e3a5eb46f3229969347a94de",
+    "url": "/lygiahan/DeloyGit.git/static/js/2.a0648e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a60d3ac9fd5ff3fe8b9",
+    "url": "/lygiahan/DeloyGit.git/static/js/main.6239809b.chunk.js"
+  },
+  {
+    "revision": "14dc7251c0712fb5f0c2",
+    "url": "/lygiahan/DeloyGit.git/static/js/runtime-main.005660af.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/lygiahan/DeloyGit.git/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/lygiahan/DeloyGit.git/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/lygiahan/DeloyGit.git/static/media/revicons.2feb69cc.eot"
+  }
+]);
